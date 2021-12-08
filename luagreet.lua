a9c7b74120c79b39a67958effdef898b8bbefbf3
kae <86919729+kaeara@users.noreply.github.com>
2021-12-09 00:34:01 +1000

    added extras

---
+++ luagreet.lua
@@ -0,0 +1,28 @@
+--[[
+This is proof of concept.
+This is not going to be finished for some time.
+--]]
+
+--[[
+This prints the starting point for the script.
+A simple introduction.
+--]]
+
+function easterEggs()
+        print "You looked through the source code, nice."
+    end
+
+print "Who is this?"
+name = io.read()
+
+if name == "Kae" then
+    print "Looking good."
+else
+if name ~= "Kae" then
+    print "Hello there."
+end      
+if name == "honor" then
+   easterEggs()
+    end
+end
+
